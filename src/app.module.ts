2d9f7ad81b8d1c0e8feca146d6479f40b1b72271
velo1203 <hokitoki1203@gmail.com>
2024-05-07 18:24:35 +0900

    feat: Add Prisma module and service
    This commit adds the Prisma module and service to the project. The Prisma module is imported in the app.module.ts file, along with the AuthModule and UserModule. The Prisma service is defined in the prisma.service.ts file and extends the PrismaClient class from the @prisma/client package. The Prisma service also implements the OnModuleInit interface and connects to the Prisma client in the onModuleInit method.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
